0289c12497f1eefed2ff7e6f15d810965fede976
c4928315 <c4928315@gmail.com>
2023-12-20 22:11:52 +0300

    on my way

--- src/Components/Forms/PostJob/postJob.js
+++ src/Components/Forms/PostJob/postJob.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import useFetch from "../../../Hooks/useFetch";
 import "./postJob.css";
 
 function PostJob() {
   const [startDate, setStartDate] = useState(new Date());
 
+  const {data: companyDropdown} = useFetch("https://efmsapi-staging.azurewebsites.net/api/Companies/getCompaniesDropDown")
+
+  console.log(companyDropdown)
   return (
     <div className="postJob">
       <div className="postForm">
         <div className="postFormInputContainer">
           <label>Company Name</label>
-          <input
-            type="text"
-            className="postFormInput"
-            placeholder="Input Value"
-          />
+           <select className="form-select postFormInput" aria-label="Default select example">
+              <option defaultValue>Select</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
         </div>
         <div className="postFormInputContainer">
           <label>Company Headquaters</label>
